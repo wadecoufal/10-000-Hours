ed89534639d58f8e40031af1d7100eb3f3baefa4
Wade Coufal <wade.coufal@gmail.com>
2018-11-18 19:03:11 -0800

    Setup index.html skeleton

---
+++ hours.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const timeBankCanvas = document.getElementById("time-bank");
+
+  timeBankCanvas.width = 1000;
+  timeBankCanvas.height = 1000;
+
+});
