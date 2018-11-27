e6c2733e1b5f8d085f4efa8906e6c2cffc79c497
Wade Coufal <wade.coufal@gmail.com>
2018-11-26 20:12:55 -0800

    Add styling to squares

--- assets/colors.js
+++ assets/colors.js
@@ -4,7 +4,32 @@ const COLORS = {
   purple: ["#e0ccff", "#d1b3ff", "#c299ff", "#b380ff", "#a366ff"]
 };
 
+// const COLORS = {
+//   green: {
+//     red: ['cc', 'b3', '99', '80', '66', '4d', '33'],
+//     green: ['ff', 'ff', 'ff', 'ff', 'ff', 'ff', 'ff'],
+//     blue: ['cc', 'b3', '99', '80', '66', '4d', '33'],
+//   },
+//   blue: {
+//     red: ['33','33','99','80','66','4d','33'],
+//     green: ['eb','e0','d6','cc','c2','b8','ad'],
+//     blue: ['ff','ff','ff','ff','ff','ff','ff'],
+//   },
+//   purple: {
+//     red: ['e0','d1','c2','b3','a3','94','85'],
+//     green: ['cc','b3','99','80','66','4d','33'],
+//     blue: ['ff','ff','ff','ff','ff','ff','ff'],
+//   },
+// }
+
+const randNum = n => {
+  return Math.floor(Math.random() * n)
+}
+
+// export const getColor = color => {
+//   return `#${COLORS[color]['red'][randNum(7)]}${COLORS[color]['green'][randNum(7)]}${COLORS[color]['blue'][randNum(7)]}`;
+// }
+
 export const getColor = color => {
-  const randNum = Math.floor(Math.random() * 5);
-  return COLORS[color][randNum];
+  return COLORS[color][randNum(5)];
 }
